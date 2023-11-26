a9117f16fd8563e9d02f075fca812aeed81c6851
Kai <kai@hannover38.de>
2023-11-26 02:34:54 +0100

    Update markdeep-slides-autoslide-plugin.js

--- markdeep-slides-autoslide-plugin.js
+++ markdeep-slides-autoslide-plugin.js
@@ -10,23 +10,7 @@ if(isActive){
         var autoslider_upcomingSlide = currentSlideNum == slideCount-1 ? 1 : currentSlideNum+1;
         if(!autoslider_repeat){autoslider_upcomingSlide = currentSlideNum+1;}
         if(autoslider_growingProgressbar){
-            autoslider_growingProgressbarStyle = `
-            .autoslider_growingProgressbar {
-                width: 100%;
-                height: 2px;
-                background-color: ${autoslider_growingProgressbarColor};
-                animation: grow ${autoslider_seconds}s 1 linear;
-                }
-
-                @keyframes grow {
-                0% {
-                    width: 0%;
-                }
-                100% {
-                    width: 100%;
-                }
-            }
-            `;
+            autoslider_growingProgressbarStyle = `.autoslider_growingProgressbar{width:100%;height:2px;background-color:${autoslider_growingProgressbarColor};animation:grow ${autoslider_seconds}s 1 linear;}@keyframes grow{0%{width:0}100%{width:100%}}`;
            var styleSheet = document.createElement("style");
            styleSheet.innerText = autoslider_growingProgressbarStyle;
            document.head.appendChild(styleSheet);
